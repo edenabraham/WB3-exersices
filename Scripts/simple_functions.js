016c79213ae9316b060c90085611fd45ab3836c1
Eden Abraham <eabraham@atl.yearup.org>
2022-10-03 12:04:58 -0400

    added simple functions exersice

---
+++ Scripts/simple_functions.js
@@ -0,0 +1,18 @@
+//define 3 name functions
+function favoriteThings() {
+    console.log("Name: Eden");
+    console.log("Favorite Movie: Rush Hour");
+    console.log("Favorite Artist: SIR");
+}
+function whyImHere() {
+    console.log("I am here because this oppurtunity can change my life.");
+}
+    
+function favoritePlaces() {
+    console.log("One of my favorite places is the beach.")
+    
+}
+//call all three functions
+favoriteThings();
+whyImHere();
+favoritePlaces()
