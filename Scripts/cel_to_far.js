f2b817965288d5c46168959be52864ec1eddc943
Eden Abraham <eabraham@atl.yearup.org>
2022-10-05 00:01:47 -0400

    added conversions between fahrenheit and celsius

---
+++ Scripts/cel_to_far.js
@@ -0,0 +1,8 @@
+function convertCtoF(currentTemp) {
+    var Celsius=currentTemp
+    var fahrenheit= (Celsius*9/5)+32;
+    console.log(fahrenheit)
+    return;
+
+}
+convertCtoF(100)
---
+++ Scripts/f_to_c.js
@@ -0,0 +1,9 @@
+
+function convertFtoC(currentTemp) {
+    var Fahrenheit=currentTemp
+    var celsius= (Fahrenheit-32)*5/9;
+    console.log(celsius)
+    return;
+
+}
+convertFtoC(212);
